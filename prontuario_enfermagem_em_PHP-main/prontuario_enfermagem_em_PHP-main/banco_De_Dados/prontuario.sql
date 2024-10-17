298da1a5fc0cd5ca2bdd4ad20fca135e03990046
Mateus <mateustlopes11@gmail.com>
2024-10-16 23:17:22 -0300

    mensagem

--- prontuario_enfermagem_em_PHP-main/prontuario_enfermagem_em_PHP-main/banco_De_Dados/prontuario.sql
+++ prontuario_enfermagem_em_PHP-main/prontuario_enfermagem_em_PHP-main/banco_De_Dados/prontuario.sql
@@ -13,3 +13,13 @@ telefone varchar(15),
 endereco varchar(100)
 );
 
+CREATE TABLE medicamento(
+    id int auto_increment primary key,
+    dados_pacientes_id int,
+    nome_medicamento VARCHAR(255) not null,
+    dosagem VARCHAR(255) not null,
+    frequencia varchar (255) not null,
+    data_inicio DATE,
+    data_fim DATE,
+    foreign key (dados_pacientes_id) REFERENCES dados_pacientes(id)
+);
